5cf116fdba5d327c175a8928a12dc338c314926b
av3r6un <av3rgun@yandex.com>
2022-11-13 19:48:20 +0300

    Changes

--- func.py
+++ func.py
@@ -1,4 +1,6 @@
 from colorama import Fore
 
+TEXT = Fore.BLUE + 'Hello, World!' + Fore.RESET
+
 print(Fore.RED + '[INFO]: Changes detected!' + Fore.RESET)
 print(Fore.BLUE + '[INFO]: Changes detected2!' + Fore.RESET)
